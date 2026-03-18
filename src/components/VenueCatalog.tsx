1859ece1f15b45361b444020e691a78724a1d232
applestz <aimpawee8524@gmail.com>
2026-03-18 23:24:02 +0700

    edit VenueCatalog

--- src/components/VenueCatalog.tsx
+++ src/components/VenueCatalog.tsx
@@ -1,14 +1,14 @@
 import Link from "next/link"
 import Card from "./Card"
 
-export default async function VenueCatalog({venueJson} : {venueJson:Object}) {
+export default async function VenueCatalog({venueJson} : {venueJson:Promise<VenueJson>}) {
     const venueJsonReady = await venueJson
     return (
         <>
             <div style={{margin:"20px", display:"flex", flexDirection:"row",
                 flexWrap:"wrap", justifyContent:"space-around", alignContent:"space-around"}}>
                     {
-                        venueJsonReady.data.map((venueItem:Object)=>(
+                        venueJsonReady.data.map((venueItem:VenueItem)=>(
                             <Link href={`/venue/${venueItem.id}`} className="w-1/5" key={venueItem.id}>
                                 <Card venueName={venueItem.name} imgSrc={venueItem.picture}/>
                             </Link>
